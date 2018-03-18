a1367cf2136a22bfa6137cd9540437336046906c
Ahmed Ayman <ahmed.ayman1708@gmail.com>
2018-03-19 01:52:04 +0200

    AboutPage

--- BakingApp/app/src/main/java/com/freelance/ahmed/bakingapp/Fragments/AboutFragment.java
+++ BakingApp/app/src/main/java/com/freelance/ahmed/bakingapp/Fragments/AboutFragment.java
@@ -10,6 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.freelance.ahmed.bakingapp.R;
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutFragment extends Fragment {
 
@@ -23,12 +28,30 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        Element versionElement = new Element();
+        versionElement.setTitle("Version 1.0");
+        View aboutPage = new AboutPage(getContext())
+                .isRTL(false)
+                .setDescription(getResources().getString(R.string.desc_about))
+                .setImage(R.drawable.bbq)
+                .addItem(versionElement)
+                .addGroup("Connect with us")
+                .addEmail("ahmed.ayman1708@gmail.com")
+                .addWebsite("http://ahmed-ayman1708.me/")
+                .addFacebook("a.ayman1996")
+                .addTwitter("Ahmeda1708")
+                .addGitHub("Ahmeda1708")
+                .create();
+        return (aboutPage);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("About Walima");
+
+
+
 
     }
 }
